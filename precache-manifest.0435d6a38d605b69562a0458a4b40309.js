14b5ac5a3a6a8f8acd560ec0b8aa1c69ebf1b6ec
Aldhair Vera Camacho <u201315853@upc.edu.pe>
2020-03-24 16:46:39 -0500

    Updates

---
+++ precache-manifest.0435d6a38d605b69562a0458a4b40309.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48b88157762d3388b833220ded6ff4e9",
+    "url": "/RecipeApp/index.html"
+  },
+  {
+    "revision": "743f79d6233e8df03f31",
+    "url": "/RecipeApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c37d4b61cb9d8b2fa5dc",
+    "url": "/RecipeApp/static/js/2.eb0a154e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RecipeApp/static/js/2.eb0a154e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "743f79d6233e8df03f31",
+    "url": "/RecipeApp/static/js/main.dd3fd09c.chunk.js"
+  },
+  {
+    "revision": "caac379da1508bc0bd0a",
+    "url": "/RecipeApp/static/js/runtime-main.44346485.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/RecipeApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
